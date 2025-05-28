c9bf816ed5779f0f12611262648c0d902ba2f55d
Manas Todi <manastodi1309@gmail.com>
2025-05-29 02:41:56 +0530

    18:20

--- frontend/finsplit/src/App.jsx
+++ frontend/finsplit/src/App.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Auth/Dashboard/Home';
+import Income from './pages/Auth/Dashboard/Income';
+import Expense from './pages/Auth/Dashboard/Expense';
+
 const App = () => {
   return (
-    <div className=''>
-      Hello
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />}/>
+          <Route path='/login' exact element={<Login />}/>
+          <Route path='/signUp' exact element={<SignUp />}/>
+          <Route path='/dashboard' exact element={<Home />}/>
+          <Route path='/income' exact element={<Income />}/>
+          <Route path='/Expense' exact element={<Expense />}/>
+        </Routes>
+      </Router>
+      
     </div>
   )
 }
 
 export default App
 
+const Root = () => {
+    // Check if token exists in localStorage
+    const isAuthenticated = !!localStorage.getItem("token");
+
+    // Redirect to dashboard if authenticated, otherwise to login
+    return isAuthenticated ? (
+        <Navigate to="/dashboard" />
+    ) : (
+        <Navigate to="/login" />
+    );
+};
